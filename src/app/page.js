80d5f7ff6239225330c8c781dd3759a82397b347
redondo <riad.reda.fethi@gmail.com>
2024-09-25 14:01:51 +0200

    modif texte  page d'acceuil

--- src/app/page.js
+++ src/app/page.js
@@ -9,19 +9,20 @@ export default function Home() {
     <div className="min-h-screen p-8 pb-20">
       
       <h1 className="text-center mt-4">home page </h1>
-      
- <p className='introduction'>
- Bienvenue sur mon  site dédié à l'histoire des meilleurs joueurs de football !
+      <p className='introduction'>
+  Bienvenue sur mon site dédié à l&apos histoire des meilleurs joueurs de football !
 
-  Ce projet, à but non lucratif, est le fruit de ma passion pour le football, une passion que j'entretiens depuis 45 ans. Au fil des années, j'ai eu la chance de suivre les exploits de nombreux joueurs qui ont marqué ce sport, tant par leur talent que par leur détermination.
+  Ce projet, à but non lucratif, est le fruit de ma passion pour le football, une passion que j&apos entretiens depuis 45 ans. Au fil des années, j&aposai eu la chance de suivre les exploits de nombreux joueurs qui ont marqué ce sport, tant par leur talent que par leur détermination.
 
-  Sur ce site, vous trouverez une sélection des meilleurs joueurs qui ont profondément marqué mon parcours, mais également ceux dont mon père m'a souvent parlé. J'ai également puisé dans les souvenirs d'émissions et de reportages captivants que j'ai visionnés, notamment sur des emmissions italiennes comme "Sfide".
+  Sur ce site, vous trouverez une sélection des meilleurs joueurs qui ont profondément marqué mon parcours, mais également ceux dont mon père m&aposa souvent parlé. J&aposai également puisé dans les souvenirs d&aposémissions et de reportages captivants que j&aposai visionnés, notamment sur des émissions italiennes comme  &quotSfide&quot.
 
-  Je suis conscient que ce choix peut contenir des erreurs et des oublis. Le football est un domaine vaste et subjectif, et chaque passionné a ses propres héros. Je m'engage à mettre à jour régulièrement cette liste afin de refléter au mieux l'évolution de ce magnifique sport.
+  Je suis conscient que ce choix peut contenir des erreurs et des oublis. Le football est un domaine vaste et subjectif, et chaque passionné a ses propres héros. Je m&aposengage à mettre à jour régulièrement cette liste afin de refléter au mieux l&aposévolution de ce magnifique sport.
 
-  Merci de partager cette aventure avec moi et n'hésitez pas à plonger dans l/univers fascinant des légendes du football !
-    "Riad reda fethi"
- </p>
+  Merci de partager cette aventure avec moi et n&apos;hésitez pas à plonger dans l&apos univers fascinant des légendes du football !
+  &quot;Riad reda fethi&quot;
+</p>
+
+
       </div>
     
   );
